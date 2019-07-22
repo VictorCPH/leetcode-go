fbef9e0a134bff4195834f386392f93b1341e239
chenpenghui <chenpenghui@sensetime.com>
2019-07-22 15:42:12 +0800

    35.search-insert-position

---
+++ 35.search-insert-position.go
@@ -0,0 +1,30 @@
+/*
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
+}
+*/
+func searchInsert(nums []int, target int) int {
+	low, high := 0, len(nums)-1
+	mid := 0
+	for low <= high {
+		mid = low + (high-low)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	if nums[mid] < target {
+		return mid + 1
+	}
+	return mid
+}
